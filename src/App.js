0090d42ae2e2b835f1b8d9fe27585377803f1782
maxlebro <z.joriot@gamil.com>
2024-01-23 15:19:59 +0100

    ajout de chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./.css/header.css";
+import "./.css/footer.css";
 
 function App() {
   return (
     <div>
-      <header role="header">
+      <header class="header">
         <p>la je mettrai un logo</p>
         <p>une belle histoire sur son point de depart</p>
         <p>la je mettrai le login</p>
       </header>
+      <footer class="footer">
+        <p>test</p>
+      </footer>
     </div>
   );
 }
