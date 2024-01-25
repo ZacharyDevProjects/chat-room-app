dbf0ae8ccaa4bf3db8d4995ae9668b34e0493528
maxlebro <z.joriot@gamil.com>
2024-01-25 11:51:37 +0100

    salut

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -9,13 +9,11 @@ export default function Chat() {
   useEffect(() => {
     async function fetchPostData() {
       try {
-        const { data, error } = await supabase
-        .from("post")
-        .select("message");
+        const { data, error } = await supabase.from("post").select("message");
         if (error) {
           throw new Error(error.message);
         }
-        console.log(data)
+        console.log(data[0].message);
         setData(data);
       } catch (error) {
         setError(error.message);
@@ -28,6 +26,13 @@ export default function Chat() {
   return (
     <Container maxW="600px" paddingBottom="20px">
       <Box bg="white" p="5" overflow="auto" borderRadius="10px">
+        {data ? (
+          data.map((item, index) => (
+          <Box key={index} bgColor={"#E5E5E5"} mt={"20px"}>{data[index].message}</Box>
+          ))
+        ) : (
+          <p>{error ? `Erreur: ${error}` : "Chargement en cours..."}</p>
+        )}
       </Box>
     </Container>
   );
