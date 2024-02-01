d8d0e74dd17129ef05f9735ef925324a020e81f7
maxlebro <z.joriot@gamil.com>
2024-02-01 09:54:07 +0100

    ajout d'input

--- 01_client/src/layout/footer.jsx
+++ 01_client/src/layout/footer.jsx
@@ -1,9 +1,45 @@
-import { Box } from "@chakra-ui/react"
+import React, { useState } from "react";
+import { Box, Input } from "@chakra-ui/react";
+// import supabase from "../supabaseClient";
 
 export default function footer() {
+  const MessageInput = () => {
+    const [message, setMessage] = useState("");
+
+    const handleKeyDown = (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        sendMessage();
+      }
+    };
+
+    const sendMessage = () => {
+      console.log("Message envoyé :", message);
+
+      setMessage("");
+    };
+
     return (
-  <Box position="fixed" bottom="0" width="100%" height="20%" display="Flex" justifyContent="center" bgColor="#2EFD3C" alignItems="center">
-    <p>test</p>
-  </Box>
-    )
+      <Box
+        position="fixed"
+        bottom="0"
+        width="100%"
+        height="20%"
+        display="Flex"
+        justifyContent="center"
+        bgColor="#2EFD3C"
+        alignItems="center"
+      >
+        <Input
+          placeholder="text"
+          width={"30rem"}
+          bgColor={"#2efd57"}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </Box>
+    );
+  };
+  return <MessageInput />;
 }
