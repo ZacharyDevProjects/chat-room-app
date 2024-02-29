9f18085fb3867463c41badf75515b56ed8af3498
maxlebro <z.joriot@gamil.com>
2024-02-29 14:45:53 +0100

    implementation d'un routeur (non fonctionnel)

--- 01_client/src/App.js
+++ 01_client/src/App.js
@@ -1,12 +1,29 @@
-import Header from "../src/components//layout/header";
+import React from "react";
+import Header from "../src/components/layout/header";
 import Footer from "../src/components/layout/footer";
 import Chat from "../src/components/chat/chat";
-export default function App() {
+import LoginPage from "../src/components/login/loginPage";
+import { Route, Router } from "react-router-dom";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Header />
+//       <Chat />
+//       <Footer />
+//     </>
+//   );
+// };
+
+const App = () => {
   return (
     <>
-      <Header />
-      <Chat />
-      <Footer />
+      <Router>
+        <Route>
+          <LoginPage />
+        </Route>
+      </Router>
     </>
   );
-}
+};
+export default App;
--- 01_client/src/index.js
+++ 01_client/src/index.js
@@ -1,18 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+} from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+const { Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <ChakraBaseProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ChakraBaseProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
